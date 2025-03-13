472d24e6cfa8cdec9ca0e464bf1333e26f05c938
SalmaanRauf <raufsalmaan@csu.fullerton.edu>
2025-03-13 15:05:25 -0700

    documentation

--- game_of_nim.py
+++ game_of_nim.py
@@ -1,15 +1,14 @@
 from games import *
 
 class GameOfNim(Game):
-    """Play Game of Nim with first player 'MAX'.
-    A state has the player to move, a cached utility, a list of moves in
-    the form of a list of (x, y) positions, and a board, in the form of
-    a list with number of objects in each row."""
+    # A state has the player to move, a cached utility, a list of moves in
+    # the form of a list of (r, n) positions, and a board as a list with 
+    # number of objects in each row.
 
     def __init__(self, board=[3,1]):
-        # Create the initial game state
-        # Player 'MAX' goes first
-        # Generate valid moves for the initial board
+        # make the initial game state
+        # player 'MAX' goes first
+        # get valid moves for the starting board
         moves = []
         for r, n_objects in enumerate(board):
             for n in range(1, n_objects + 1):
@@ -17,37 +16,33 @@ class GameOfNim(Game):
         self.initial = GameState(to_move='MAX', utility=0, board=board, moves=moves)
 
     def actions(self, state):
-        """Legal moves are at least one object, all from the same row."""
+        # legal moves are at least one object, all from the same row
         return state.moves
 
     def result(self, state, move):
-        """Return the state that results from making a move from a state."""
-        # Create a new board by copying the current one
+        # return the state that results from making a move from a state
+        # copy the current board
         board = state.board.copy()
         r, n = move
         board[r] -= n
         
-        # Generate new valid moves
+        # get new valid moves
         moves = []
         for r, n_objects in enumerate(board):
             for n in range(1, n_objects + 1):
                 moves.append((r, n))
         
-        # Toggle player
         next_player = 'MIN' if state.to_move == 'MAX' else 'MAX'
-        
-        # Return new state
         return GameState(to_move=next_player, utility=0, board=board, moves=moves)
 
     def utility(self, state, player):
-        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
         if self.terminal_test(state):
-            # The player who is about to move in a terminal state is the loser
+            # player who has to move in terminal state loses
             return -1 if state.to_move == player else 1
         return 0
 
     def terminal_test(self, state):
-        """A state is terminal if there are no objects left"""
+        # a state is terminal if there are no objects left
         return sum(state.board) == 0
 
     def display(self, state):
@@ -56,12 +51,12 @@ class GameOfNim(Game):
 
 
 if __name__ == "__main__":
-    nim = GameOfNim(board=[0, 5, 3, 1]) # Creating the game instance
-    #nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
-    print(nim.initial.board) # must be [0, 5, 3, 1]
-    print(nim.initial.moves) # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 1), (2, 2), (2, 3), (3, 1)]
-    print(nim.result(nim.initial, (1,3) ))
-    utility = nim.play_game(alpha_beta_player, query_player) # computer moves first 
+    nim = GameOfNim(board=[0, 5, 3, 1]) # making the game
+    #nim = GameOfNim(board=[7, 5, 3, 1]) # bigger board to search for more testing
+    print(nim.initial.board)
+    print(nim.initial.moves)
+    print(nim.result(nim.initial, (1,3)))
+    utility = nim.play_game(alpha_beta_player, query_player)
     if (utility < 0):
         print("MIN won the game")
     else:
